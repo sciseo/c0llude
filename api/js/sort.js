1fcd866a2d5fc7db2ebd50aef9985f789d135dd7
Nefarious Laboratories <neflabs@users.noreply.github.com>
2018-04-17 19:31:58 -0400

    Create sort.js

---
+++ api/js/sort.js
@@ -0,0 +1,6 @@
+var main = document.getElementById( 'sortList' );
+[].map.call( main.children, Object ).sort( function ( a, b ) {
+    return +a.id.match( /\d+/ ) - +b.id.match( /\d+/ );
+}).forEach( function ( elem ) {
+    main.appendChild( elem );
+});
